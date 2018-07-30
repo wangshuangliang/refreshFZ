36d4ea6149d533c6290d3fdfdd644afbc7b927bd
wsl530syl515 <1397871472@qq.com>
2018-07-30 09:38:08 +0800

    Initial Commit

---
+++ TestObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestObject
+//
+//  Created by wsl 王 on 2018/7/30.
+//  Copyright © 2018年 wsl 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestObject
+//
+//  Created by wsl 王 on 2018/7/30.
+//  Copyright © 2018年 wsl 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
